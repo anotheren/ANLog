2a5fd27560ccd83a82d59f7e73de365d6673e08f
liudong <liudong.edward@gmail.com>
2018-12-26 19:14:01 +0800

    初始化项目

---
+++ Sources/ANLog.h
@@ -0,0 +1,19 @@
+//
+//  ANLog.h
+//  ANLog
+//
+//  Created by 刘栋 on 2018/12/26.
+//  Copyright © 2018 anotheren.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ANLog.
+FOUNDATION_EXPORT double ANLogVersionNumber;
+
+//! Project version string for ANLog.
+FOUNDATION_EXPORT const unsigned char ANLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ANLog/PublicHeader.h>
+
+
